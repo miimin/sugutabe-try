6784217f13c3ef2e941b712d6e39826d7b0606e1
miimin <79664724+miimin@users.noreply.github.com>
2022-01-22 01:39:58 +0900

    Update index.js

--- assets/css/index.js
+++ assets/css/index.js
@@ -1,5 +1,29 @@
 $(function () {
 
+  $(".hamburger").click(function () {
+    $(this).toggleClass('hamburger-active');
+    $(".hamburger-menu").toggleClass("menu-active");
+    //$('ul li').hide(); //隠しておいて
+    //   $('ul li').each(function (i) {
+    //    $(this).delay(80 * i).fadeIn(500); //それぞれフェードイン
+    //});
+  });
+  $(".hamburger-menu a").click(function () {
+    $(".hamburger").removeClass('hamburger-active');
+    $(".hamburger-menu").removeClass('menu-active'); //ナビゲーションのpanelactiveクラスも除去
+  });
+  $("header a,.tomato-button a").click(function () {
+    var id = $(this).attr("href");
+    var header_height = $("header").height();
+    var adjust = 0; //px分スラしたくなったら数字入れる
+    var position = $(id).offset().top - header_height + adjust;
+    $("html, body").animate({
+      "scrollTop": position
+    }, 500, "swing");
+    return false;
+  });
+
+
   const img_src = ["assets/images/daikon.png", "assets/images/paprika.png", "assets/images/corn.png", "assets/images/potato.png", "assets/images/tomato.png"];
   let number = -1;
 
@@ -11,30 +35,34 @@ $(function () {
       number++;
     }
     document.getElementById("top-slide").src = img_src[number];
-  }
-
-  $(function () {
-    var script = $('<script>').attr({
-      'type': 'text/javascript',
-      'src': 'assets/css/graph-data.json'
-    });
-    $('body')[0].appendChild(script[0]);
-  });
-  //ファイルの記述がおかしいエラーが出る。わかめ
-
-
-  /*
-  var animation = bodymovin.loadAnimation({
-    container: document.getElementById('lottie-graph'),
-    renderer: 'svg', // 描画形式
-    loop: false, // trueにしたらループ。1回再生の場合はfalse
-    autoplay: true, // 自動再生
-    path: 'graph-data.json'
-  });
-  var anim = lottie.loadAnimation(params);
-  */
+  };
 
   setInterval(slide_time, 3000);
+  //
+  //
+  //
+
+
+  //
+  $(function () {
+    $(".section-season__list-item").click(function () {
+      $(".season-beg", this).slideToggle();
+      var $name = $(this).find(".season-name");
+      var $beg = $(this).find(".season-beg");
+      if ($beg.hasClass("open")) { //.Aがopenを持っている
+        $beg.removeClass("open"); //.openを消す       
+        $(this).find(".triafter").removeClass("triafter");
+        $(this).find(".spring-triafter").removeClass("spring-triafter"); //下三角にする
+      } else { //.Aがopenを持たない
+        $beg.addClass("open");
+        $(this).find(".triangle").addClass("triafter");
+        $(this).find(".spring-triangle").addClass("spring-triafter"); //上三角にする
+      }
+    });
+  });
+  //
+
+  //
   //QA押したとき
   $(".section-QA__list-item").click(function () {
     var $answer = $(this).find(".A");
@@ -54,17 +82,69 @@ $(function () {
       }, 10); //Q下の角丸解除
       $(this).find(".triangle").addClass("triafter"); //上三角にする
     }
-  })
-
-  $("header a,.tomato-button a").click(function () {
-    var id = $(this).attr("href");
-    var header_height = $("header").height();
-    var adjust = 0; //px分スラしたくなったら数字入れる
-    var position = $(id).offset().top - header_height + adjust;
-    $("html, body").animate({
-      "scrollTop": position
-    }, 500, "swing");
-    return false;
   });
 
+
+
+
+});
+
+$(function () {
+
+  $('#regular-sp').slick({
+    dots: false,
+    accessibility: true,
+    pauseOnHover: true,
+    autoplay: false,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    focusOnSelect: false,
+    adaptiveHeight: true,
+    prevArrow: '<img src="assets/images/arrowleft.svg" class="slide-arrow prev-arrow" alt="前へ">',
+    nextArrow: '<img src="assets/images/arrowright.svg" class="slide-arrow next-arrow" alt="次へ">',
+  });
+  $('#farmar-sp').slick({
+    dots: true,
+    accessibility: true,
+    pauseOnHover: true,
+    autoplay: false,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    focusOnSelect: false,
+    adaptiveHeight: true,
+    prevArrow: '<img src="assets/images/arrowleft.svg" class="slide-arrow prev-arrow" alt="前へ">',
+    nextArrow: '<img src="assets/images/arrowright.svg" class="slide-arrow next-arrow" alt="次へ">',
+  });
+  $('#voice-sp').slick({
+    dots: true,
+    accessibility: true,
+    pauseOnHover: true,
+    autoplay: false,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    focusOnSelect: false,
+    adaptiveHeight: true,
+    prevArrow: '<img src="assets/images/arrowleft.svg" class="slide-arrow prev-arrow" alt="前へ">',
+    nextArrow: '<img src="assets/images/arrowright.svg" class="slide-arrow next-arrow" alt="次へ">',
+  });
+
+  //↓360以下のときviewportを書き換えるやつ らしい
+  !(function () {
+    const viewport = document.querySelector('meta[name="viewport"]');
+
+    function switchViewport() {
+      const value =
+        window.outerWidth > 360 ?
+        'width=device-width,initial-scale=1' :
+        'width=360';
+      if (viewport.getAttribute('content') !== value) {
+        viewport.setAttribute('content', value);
+      }
+    }
+    addEventListener('resize', switchViewport, false);
+    switchViewport();
+  })();
 });
